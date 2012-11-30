99f499ef013735dfe9cf9c302a74f1746e611151
Andrew Kuznetsov <kuznetsov.miet@gmail.com>
2012-12-01 02:17:44 +0400

    Add tests

---
+++ materials/test/refactoring/CustomerTest.java
@@ -0,0 +1,42 @@
+package refactoring;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class CustomerTest extends TestCase
+{
+	private Customer customer;
+	private Movie movie;
+	private Rental rental;
+	
+	@Before
+	public void setUp(){
+		movie = new Movie("MovieName", 100);
+		
+		rental = new Rental(movie, 7);
+		
+		customer = new Customer("CustomerName");
+	}
+	
+	@Test
+	public void testStatement()
+	{
+		customer.addRental(rental);
+		assertEquals(
+				"Учет аренды для CustomerName\n	MovieName	0.0\nСумма задолженности составляет 0.0\nВы заработали 1 очков за активность",
+				customer.statement());
+	}
+	
+	@Test
+	public void testRegularMovieStatement()
+	{
+		movie.setPriceCode(Movie.REGULAR);
+		customer.addRental(rental);
+		assertEquals(
+				"Учет аренды для CustomerName\n	MovieName	9.5\nСумма задолженности составляет 9.5\nВы заработали 1 очков за активность",
+				customer.statement());
+	}
+
+}
